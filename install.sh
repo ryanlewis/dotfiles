9d6fe3a54988768fff7732cec3ce6a6f1fb43d5f
Ryan Lewis <ryan@wpyz.org>
2014-05-20 00:30:23 +0100

    Updates to install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,16 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.dotfiles/prezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+# Install oh my zsh
+curl -L http://install.ohmyz.sh | sh
+
+# Install dotfiles
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
+
+# Remove zshrc, link to ours
+rm ~/.zshrc
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+
+# Install vim bundles
+vim +PluginInstall +qall
