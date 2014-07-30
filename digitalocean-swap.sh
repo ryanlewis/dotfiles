438084671bb2b9cd6a29484f21c0bcd0304f21d1
Ryan Lewis <ryan@wpyz.org>
2014-07-30 13:56:45 +0100

    Added script to setup swap on digitalocean VMs

---
+++ digitalocean-swap.sh
@@ -0,0 +1,14 @@
+swapsize=${1-2G}
+
+fallocate -l $swapsize /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+echo "/swapfile none swap sw 0 0" >> /etc/fstab
+
+sysctl vm.swappiness=10
+echo "vm.swappiness=10" >> /etc/sysctl.conf
+
+sysctl vm.vfs_cache_pressure=50
+echo "sysctl vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
