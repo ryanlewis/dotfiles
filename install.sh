219dd1d85af7d363aa0054b8806ea122d6eff2da
Ryan Lewis <ryan@wpyz.org>
2014-07-30 06:11:48 -0400

    install.sh inits submodules, builds YCM

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.dotfiles/prezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+# Install some build stuff (YouCompleteMe needs this)
+apt-get install -y build-essential cmake python-dev
+
 # Install oh my zsh
 curl -L http://install.ohmyz.sh | sh
 
@@ -15,5 +18,12 @@ ln -s ~/.dotfiles/vim ~/.vim
 rm ~/.zshrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 
+# Init submodules 
+git submodule update --init --recursive
+
 # Install vim bundles
 vim +PluginInstall +qall
+
+# Compile YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe
+./install.sh
