24af6965396fe95be08116f03c196882360699da
Ryan Lewis <ryan@rlew.io>
2026-05-08 13:13:31 +0100

    Use nerd font glyphs for fish vim mode indicator
    Replace bracketed letters ([N]/[I]/[R]/[V]) with semantic glyphs:
    lock for normal, pencil for insert, refresh for replace, eye for
    visual. UTF-8 byte escapes keep the source ASCII-safe.

--- private_dot_config/private_fish/functions/fish_mode_prompt.fish
+++ private_dot_config/private_fish/functions/fish_mode_prompt.fish
@@ -1,14 +1,14 @@
 function fish_mode_prompt
     switch $fish_bind_mode
         case default
-            echo -en "\e[1;31m[N]\e[0m "
+            echo -en "\e[1;31m\xef\x80\xa3\e[0m  "
         case insert
-            echo -en "\e[1;32m[I]\e[0m "
+            echo -en "\e[1;32m\xef\x81\x80\e[0m  "
         case replace_one
-            echo -en "\e[1;33m[R]\e[0m "
+            echo -en "\e[1;33m\xef\x80\xa1\e[0m  "
         case visual
-            echo -en "\e[1;35m[V]\e[0m "
+            echo -en "\e[1;35m\xef\x81\xae\e[0m  "
         case '*'
-            echo -en "\e[1;37m[?]\e[0m "
+            echo -en "\e[1;37m?\e[0m  "
     end
 end
